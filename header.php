095e80dcda5a69af9ce6afa668345435efc9419e
kirandash <thekirankumardash@gmail.com>
2016-09-20 18:25:43 +0530

    Add an Optional Header Image Function

--- header.php
+++ header.php
@@ -24,6 +24,13 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'amuse' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
+		<?php if ( get_header_image() ) : ?>
+        <div class="header-image">
+            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+                <img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
+            </a>
+        </div>
+        <?php endif; // End header image check. ?>
 		<div class="site-branding">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<?php
--- inc/custom-header.php
+++ inc/custom-header.php
@@ -24,9 +24,9 @@ function amuse_custom_header_setup() {
 	add_theme_support( 'custom-header', apply_filters( 'amuse_custom_header_args', array(
 		'default-image'          => '',
 		'default-text-color'     => '000000',
-		'width'                  => 1000,
+		'width'                  => 1600,
 		'height'                 => 250,
-		'flex-height'            => true,
+		'flex-height'            => false,
 		'wp-head-callback'       => 'amuse_header_style',
 	) ) );
 }
